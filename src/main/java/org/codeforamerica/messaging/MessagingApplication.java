28f83f15a1da8906d3d3bd02a7528975974d864a
Vraj Mohan <vraj@codeforamerica.org>
2023-03-30 10:18:38 -0700

    Start messaging project

---
+++ src/main/java/org/codeforamerica/messaging/MessagingApplication.java
@@ -0,0 +1,13 @@
+package org.codeforamerica.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingApplication.class, args);
+    }
+
+}
