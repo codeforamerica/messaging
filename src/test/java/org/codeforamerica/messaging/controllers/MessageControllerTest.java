2bead4546738ba6e8bb37f6506a44b81db879d4a
Vraj Mohan <vraj@codeforamerica.org>
2023-04-20 13:40:13 -0700

    Set up Basic Authentication

--- src/test/java/org/codeforamerica/messaging/controllers/MessageControllerTest.java
+++ src/test/java/org/codeforamerica/messaging/controllers/MessageControllerTest.java
@@ -1,5 +1,6 @@
 package org.codeforamerica.messaging.controllers;
 
+import org.codeforamerica.messaging.config.SecurityConfiguration;
 import org.codeforamerica.messaging.models.Message;
 import org.codeforamerica.messaging.repositories.MessageRepository;
 import org.codeforamerica.messaging.services.SmsService;
@@ -8,14 +9,22 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(MessageController.class)
+@Import(SecurityConfiguration.class)
 public class MessageControllerTest {
 
     @Autowired
@@ -26,6 +35,24 @@ public class MessageControllerTest {
     private MessageRepository messageRepository;
 
     @Test
+    public void getMessageUnauthenticated() throws Exception {
+        mockMvc.perform(get("/api/v1/messages/1"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    public void getMessageAuthenticated() throws Exception {
+        Mockito.when(smsService.getMessage(any()))
+                .thenReturn(Optional.of(Message.builder().providerMessageId("message_id").build()));
+
+        mockMvc.perform(get("/api/v1/messages/1")
+                        .with(httpBasic("user", "password")))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @WithMockUser
     public void createMessageSuccess() throws Exception {
         String requestBody = """
                         {
@@ -45,6 +72,7 @@ public class MessageControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void createMessageEmptyPayload() throws Exception {
         mockMvc.perform(post("/api/v1/messages")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -54,6 +82,7 @@ public class MessageControllerTest {
     }
 
     @Test
+    @WithMockUser
     public void createMessageBadPhoneNumber() throws Exception {
         String requestBody = """
                         {
