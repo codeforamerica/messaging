b5bb983694934db5a2473d6c07129e6ffe91ba54
Vraj Mohan <vraj@codeforamerica.org>
2023-05-19 18:12:20 -0700

    Create CSVReader util using Apache Commons CSV

---
+++ src/main/java/org/codeforamerica/messaging/utils/CSVReader.java
@@ -0,0 +1,32 @@
+package org.codeforamerica.messaging.utils;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CSVReader {
+    private final CSVParser parser;
+
+    public CSVReader(Reader reader) throws IOException {
+        var csvFormat = CSVFormat.Builder.create(CSVFormat.RFC4180)
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setIgnoreSurroundingSpaces(true)
+                .build();
+        this.parser =  csvFormat.parse(reader);
+    }
+
+    public boolean validateHeader(List<String> requiredHeaderNames) {
+        return CollectionUtils.isEqualCollection(requiredHeaderNames, parser.getHeaderNames());
+    }
+
+    public Stream<CSVRecord> stream() {
+        return parser.stream();
+    }
+}
---
+++ src/test/java/org/codeforamerica/messaging/utils/CSVReaderTest.java
@@ -0,0 +1,42 @@
+package org.codeforamerica.messaging.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVReaderTest {
+
+    String testCSV = """
+            phone, email, language, color, score
+            1234567890,bar@example.org, en, grey, 23
+            8885551212,foo@example.com, es, black, 42
+            """;
+
+    @Test
+    public void whenRequiredHeadersPresent_ThenSucceedsValidation() throws IOException {
+        CSVReader reader = new CSVReader(new StringReader(testCSV));
+
+        assertTrue(reader.validateHeader(List.of("phone", "email", "language", "color", "score")));
+    }
+
+    @Test
+    public void whenRequiredHeadersAbsent_ThenFailsValidation() throws IOException {
+        CSVReader reader = new CSVReader(new StringReader(testCSV));
+
+        assertFalse(reader.validateHeader(List.of("phone", "language", "color", "score")));
+    }
+
+    @Test
+    public void whenParsed_ThenReturnsCorrectCSV() throws IOException {
+        CSVReader reader = new CSVReader(new StringReader(testCSV));
+
+        List<String> result = reader.stream().map((r) -> r.get("phone")).collect(Collectors.toList());
+
+        assertEquals(result, List.of("1234567890", "8885551212"));
+    }
+}
