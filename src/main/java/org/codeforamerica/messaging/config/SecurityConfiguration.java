274512f7deeb8be18157857da46ebe77c734eb17
Mike Rotondo <mrotondo@gmail.com>
2023-05-23 16:35:25 -0700

    Turn on IP Filtering

--- src/main/java/org/codeforamerica/messaging/config/SecurityConfiguration.java
+++ src/main/java/org/codeforamerica/messaging/config/SecurityConfiguration.java
@@ -1,22 +1,31 @@
 package org.codeforamerica.messaging.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Value("${allowed-ip-addresses}")
+    String allowedIpAddresses;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/**").authenticated()
+                        .requestMatchers("/api/v1/**").access(new WebExpressionAuthorizationManager(createExpressionString()))
                         .requestMatchers("/error/**").authenticated()
                         .requestMatchers("/mailgun_callbacks/**").permitAll()
                         .requestMatchers("/twilio_callbacks/**").permitAll()
@@ -27,5 +36,17 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    /**
+     Example Output: "isAuthenticated() and (hasIpAddress('127.0.0.1') or hasIpAddress('::1'))"
+     */
+    private String createExpressionString() {
+        return Arrays.stream(allowedIpAddresses.split(","))
+                .map(String::strip)
+                .collect(Collectors.joining(
+                        "') or hasIpAddress('",
+                        "isAuthenticated() and (hasIpAddress('",
+                        "'))"));
     }
 }
