b71f075317482ef60c47b0aac971caac861ae3fb
Vraj Mohan <vraj@codeforamerica.org>
2023-06-01 16:47:00 -0700

    Listen on a different port for trusted endpoints

---
+++ src/main/java/org/codeforamerica/messaging/config/WebConfig.java
@@ -0,0 +1,23 @@
+package org.codeforamerica.messaging.config;
+
+import org.codeforamerica.messaging.config.filters.TrustedEndpointsFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${server.trustedPort:null}")
+    private String trustedPort;
+
+    @Value("${server.trustedPathPrefix:null}")
+    private String trustedPathPrefix;
+
+    @Bean
+    public FilterRegistrationBean<TrustedEndpointsFilter> trustedEndpointsFilter() {
+        return new FilterRegistrationBean<>(new TrustedEndpointsFilter(trustedPort, trustedPathPrefix));
+    }
+}
