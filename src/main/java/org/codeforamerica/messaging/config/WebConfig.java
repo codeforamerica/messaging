045a75d7ca03539460eb4832bf9ff25918b77043
Martha Pidcock <mpidcock@codeforamerica.org>
2023-07-09 12:21:17 -0700

    Implement Twilio Signature Validation in the controller instead of a Spring WebConfig filter
    Implement Twilio Signature Validation in the controller instead of a Spring WebConfig filter

--- src/main/java/org/codeforamerica/messaging/config/WebConfig.java
+++ src/main/java/org/codeforamerica/messaging/config/WebConfig.java
@@ -1,12 +1,10 @@
 package org.codeforamerica.messaging.config;
 
 import org.codeforamerica.messaging.config.filters.TrustedEndpointsFilter;
-import org.codeforamerica.messaging.config.filters.TwilioRequestValidatorFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,20 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${server.trustedPathPrefix:null}")
     private String trustedPathPrefix;
 
-    @Value("${twilio.auth.token}")
-    private String twilioAuthToken;
-
     @Bean
     public FilterRegistrationBean<TrustedEndpointsFilter> trustedEndpointsFilter() {
         return new FilterRegistrationBean<>(new TrustedEndpointsFilter(trustedPort, trustedPathPrefix));
-    }
-
-    @Bean
-    @Profile("!test && !ci")
-    public FilterRegistrationBean<TwilioRequestValidatorFilter> twilioRequestValidatorFilter() {
-        FilterRegistrationBean<TwilioRequestValidatorFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new TwilioRequestValidatorFilter(twilioAuthToken));
-        filterRegistrationBean.addUrlPatterns("/public/twilio_callbacks/*");
-        return filterRegistrationBean;
     }
 }
--- src/main/java/org/codeforamerica/messaging/providers/twilio/TwilioCallbackController.java
+++ src/main/java/org/codeforamerica/messaging/providers/twilio/TwilioCallbackController.java
@@ -1,16 +1,15 @@
 package org.codeforamerica.messaging.providers.twilio;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.codeforamerica.messaging.models.SmsMessage;
 import org.codeforamerica.messaging.repositories.SmsMessageRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/public/twilio_callbacks")
@@ -18,16 +17,23 @@ import java.util.Map;
 public class TwilioCallbackController {
 
     private final SmsMessageRepository smsMessageRepository;
+    private final TwilioSignatureVerificationService twilioSignatureVerificationService;
 
-    public TwilioCallbackController(SmsMessageRepository smsMessageRepository) {
+    public TwilioCallbackController(SmsMessageRepository smsMessageRepository,
+            TwilioSignatureVerificationService twilioSignatureVerificationService) {
         this.smsMessageRepository = smsMessageRepository;
+        this.twilioSignatureVerificationService = twilioSignatureVerificationService;
     }
 
     @PostMapping(path = "/status", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public ResponseEntity<Object> updateStatus(@RequestParam Map<String, String> twilioStatusMessage) {
-        SmsMessage smsMessage = smsMessageRepository.findFirstByProviderMessageId(twilioStatusMessage.get("MessageSid"));
-        smsMessage.setStatus(twilioStatusMessage.get("MessageStatus"));
-        smsMessage.setFromPhone(twilioStatusMessage.get("From"));
+    public ResponseEntity<Object> updateStatus(HttpServletRequest request) {
+        if (!twilioSignatureVerificationService.verifySignature(request)) {
+            log.error("Signature verification failed");
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+        SmsMessage smsMessage = smsMessageRepository.findFirstByProviderMessageId(request.getParameter("MessageSid"));
+        smsMessage.setStatus(request.getParameter("MessageStatus"));
+        smsMessage.setFromPhone(request.getParameter("From"));
         smsMessageRepository.save(smsMessage);
         return ResponseEntity.ok().build();
     }
