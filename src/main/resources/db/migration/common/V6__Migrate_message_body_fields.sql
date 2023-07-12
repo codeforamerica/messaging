5cd2cb8f7720c15b9f3a094ac5f3d8e134c82eac
Martha Pidcock <mpidcock@codeforamerica.org>
2023-07-12 14:28:11 -0700

    Copy body to email_body when migrating to new column names

--- src/main/resources/db/migration/common/V6__Migrate_message_body_fields.sql
+++ src/main/resources/db/migration/common/V6__Migrate_message_body_fields.sql
@@ -3,9 +3,13 @@ alter table message
 alter table message
     alter column sms_body drop not null,
     add column if not exists email_body text;
+update message
+    set email_body=sms_body;
 
 alter table template_variant
     rename column body to sms_body;
 alter table template_variant
     alter column sms_body drop not null,
     add column if not exists email_body text;
+update template_variant
+    set email_body=sms_body;
